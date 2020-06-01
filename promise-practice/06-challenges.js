69bffeb57a30fcae98eb9db19883ef91f9bc7511
charlerae <contactcharlerae@gmail.com>
2020-05-31 23:59:57 -0400

    adds wordWithinArray challenge to 06-

--- promise-practice/06-challenges.js
+++ promise-practice/06-challenges.js
@@ -3,6 +3,7 @@
 
 // average of 3
 function averageOfThree(num1, num2, num3) {
+    if (typeof(arguments) !== 'number') return 'Input must be an integer';
     return (num1 + num2 + num3) / 2;
   }
 
@@ -23,3 +24,17 @@ console.log(eitherStringIncluded("how now brown cow?", "panther", "cow")); // tr
 console.log(eitherStringIncluded("Question?", "Answer", "?")); // true
 console.log(eitherStringIncluded("I love programming", "apple", "potato")); // false
 console.log(eitherStringIncluded("Dance party!", "?", "dance")); // true
+
+
+// returns true if that string is located inside of the array, if the string is not inside of the array, the function should return false
+function wordWithinArray(array, word) {
+  if (array.indexOf(word) === -1) {
+    return false;
+      }
+  return true;
+}
+
+console.log(wordWithinArray(["apple", "banana", "caramel", "chocolate"], "apple")); // true
+console.log(wordWithinArray(["dog", "cat", "camel", "bird"], "camel")); // true
+console.log(wordWithinArray(["apple", "banana", "caramel", "chocolate"], "pineapple")); // false
+console.log(wordWithinArray(["dog", "cat", "camel", "bird"], "panther")); // false
